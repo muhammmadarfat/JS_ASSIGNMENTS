d7eccd502f8277c06cf2c964203e97033f32eab0
muhammmadarfat <muhammadarfat1411@gmail.com>
2025-10-28 01:16:55 +0500

    my-new-branch

---
+++ Guess num game/app.js
@@ -0,0 +1,13 @@
+
+let num = prompt("enter your number")
+
+if(num == 6){
+    console.log("correct")
+}
+
+else if (num == 5){
+    console.log("only one step more")
+}
+else {
+    console.log("Try Again")
+}
