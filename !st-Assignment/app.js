1c01d53548c5e666237e66348ab19c85262e997b
muhammmadarfat <muhammadarfat1411@gmail.com>
2025-10-27 23:53:09 +0500

    add files

---
+++ !st-Assignment/app.js
@@ -0,0 +1,109 @@
+console.log("Hello, World!");
+alert("Hello,World!")
+console.log("----------------Chap 1------------------");
+//chapter 1//
+// ques 1//
+// Answer 1//
+
+alert("Welcome to JavaScript");//
+console.log("Welcome to JavaScript");
+
+// ques 2//
+// Answer 2//
+alert("Error! Please enter a valid password.");
+console.log("Error! Please enter a valid password.");
+
+// ques 3//
+// Answer 3//
+alert("Welcome to JS land...\n Happy Coding!");
+console.log("Welcome to JS land...\n Happy Coding!");
+
+// ques 4//
+// Answer 4//
+alert("Welcome to Js land...");
+alert("Happy Coding! \n Prevent this page from creating additional dialogs.");
+console.log("Welcome to Js land...");
+console.log("Happy Coding! \n Prevent this page from creating additional dialogs.");
+// ques 5//
+// Answer 5//
+console.log("Hello... I can run JS through my web browser's console");
+
+console.log("----------------Chap 2------------------");
+//chapter 2//
+//ques 1 //
+//Answer 1//
+var username;
+console.log(username);
+
+//ques 2//
+//Answer 2//
+let myName= "MUhammad Arfat";
+console.log(myName);
+
+//ques 3//
+//Answer 3//
+ var message="Hello World";
+ //alert(message);//
+
+//ques 4//
+//Answer 4//
+var name=" Jhone Doe";
+var age=" 15 years old";
+var course=" Certified Mobile Application Development";
+console.log(name);
+console.log(age);
+console.log(course);
+alert(name);//
+alert(age);//
+alert(course);//
+
+//ques 5//
+//Answer 5//
+var food= " Pizza \n Pizz \n Piz \n Pi \n P";
+alert(food);//
+console.log(food);
+
+//ques 6//
+//Answer 6//
+var email="My email address is \nmuhammadarfat1411@gmail.com"
+console.log(email);
+alert(email);//
+
+//ques 7//
+//Answer 7//
+var book="I am trying to learn from the Book A smarter way to learn JavaScript";
+alert(book);//
+console.log(book);
+
+//ques 8//
+//Answer 8//
+document.write( " <h1>Yah I can write HTML content through JavaScript </h1>");
+
+//chapter 3//
+
+//ques 1//
+//Answer 1//
+var age="I am 15 years old";
+alert(age);//
+console.log(age);
+
+//Answer 2//
+var N="15"
+var visit="You have visited this site 15 times";
+alert(visit);//
+console.log(visit);
+
+
+//Answer 3//
+var birthYear=2010;
+document.write(" <h2>+My birth year is "+birthYear+"<br> Data type of my declared variable is number </h2>");
+console.log(birthYear);
+
+//ques 4//
+//Answer 4//
+var visitorName="John Doe";
+var producttitle="T-shirt(s)";
+var quantity="5";
+var store="J."
+document.write( " <h3>"+ visitorName+ " ordered "+quantity+""+producttitle+" on "+store+" Clothing store </h3>")
+console.log(visitorName,quantity,producttitle,store)
---
+++ 2nd Assignment/app.js
@@ -0,0 +1,126 @@
+
+
+// chapter4 //
+document.writeln("<h1> Rules for naming js variables </h1>")
+let variables = "Variables name can only contain, numbers, letters,$ and_:for example: $lmyfirst_variable"
+let variables1 = " Variables must begin with a letter,$or _. forexample:$name, _name or name"
+let variables2 = "Variables names are case sensitive"
+let variables3 = "Variables should not be JS keywords"
+document.writeln("<br>"  + variables + "</br>" )
+document.writeln( "<br>"  + variables1 + "</br>")
+document.writeln( "<br>"  + variables2 + "</br>")
+ 
+// chapter5//
+// Q1 to Q3 
+// addition //
+let numers = 3
+let numers1 =5
+let Ans = numers + numers1
+document.writeln(  Ans )
+
+
+// addition //
+
+let numBers = 3
+let numBers1 =5
+let ansWer = numBers + numBers1
+document.writeln(  ansWer )
+
+// substraction //
+
+let num = 3
+let num1 =5
+let ans = num - num1
+document.writeln(  ans)
+
+// multiplication //
+
+let numbs = 3
+let nums1 =5
+let answ = numbs * nums1
+document.writeln( answ)
+
+//  division //
+
+let nubers = 27
+let nubers1 =3
+let answEr = nubers / nubers1
+document.writeln(  answEr )
+
+//  remainder //
+
+let numbers = 27
+let numbers1 =3
+let answer = numbers % numbers1
+document.writeln(  answer )
+
+// Q4 //
+var ticket1 = "600"
+var ticket5 = "5"
+var totalvalue = ticket1 * ticket5
+document.writeln( "Total value of 5 ticket to a movie is  " + totalvalue +  "<br>"  )
+
+// Q5 //
+document.writeln( "Table of 4" + "<br>")
+document.writeln("4x1=4" + "<br>")
+document.writeln("4x2=8" + "<br>")
+document.writeln("4x3=12" + "<br>")
+document.writeln("4x4=16" + "<br>")
+document.writeln("4x5=20" + "<br>")
+document.writeln("4x6=24" + "<br>")
+document.writeln("4x7=28" + "<br>")
+document.writeln("4x8=32" + "<br>")
+document.writeln("4x9=36" + "<br>")
+document.writeln("4x10=40" + "<br>")
+// Q6 //
+
+document.writeln( " 25C is 77F" + "<br>" )
+document.writeln( "70F is 21.11111111111 C")
+
+// Q 7 //
+
+document.writeln("<h1>Shoping Cart </h1>")
+document.writeln("<h2> Price of item 1 is 650 </h2>")
+document.writeln("<h2> Quality of item 1 is 3 </h2>")
+document.writeln("<h2> Price of item 2 is 200 </h2>")
+document.writeln("<h2> Quantity of item 1 is 7 </h2>")
+document.writeln("<h2> Shopping charges is 100 </h2>")
+document.writeln("<h2> Total cost of your Order is 2750</h2>")
+
+// Q8 //
+
+document.writeln("<h1> Marks sheet</h1>")
+document.writeln("<h2> Total marks = 980 </h2>")
+document.writeln("<h2> Marks Obtained = 804 </h2>")
+document.writeln("<h2> Percentage = 82.040812345287 </h2>")
+
+// Q9 //
+
+document.writeln("<h1>Currency in PKR </h1>")
+var  usdollar = 100
+var PKR = 300
+var Currencyexchange = usdollar * PKR
+document.writeln( " Total currency Exchange is =" + Currencyexchange)
+
+// Q10 and Q11//
+ document.writeln("<h1> Age Calculator</h1>")
+ document.writeln("<h2> Current Year= 2010 </h2>")
+document.writeln("<h2> Date of Birth= 1980 </h2>")
+document.writeln("<h2> Your Age= 30 </h2>")
+ 
+// Q12 //
+
+ document.writeln("<h1> The Geometrizer</h1>")
+ document.writeln("<h2> Radius of a circle:20</h2>")
+document.writeln("<h2> The circumference is:125.67999999999</h2>")
+document.writeln("<h2> The area is :1256.8</h2>")
+
+// Q13 //
+ document.writeln("<h1>The life time Supply Calculator</h1>")
+ document.writeln("<h2> Favourite snack:choclate chip </h2>")
+document.writeln("<h2> Current age :15 </h2>")
+document.writeln("<h2> Estimated maxmimum age:65</h2>")
+document.writeln("<h2> Amount of Snack per day:3</h2>")
+document.writeln("<h2> You will need 150 choclate chip to last you until to the ripe old age 65</h2>")
+
+
