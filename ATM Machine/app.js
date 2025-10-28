b95d4e3068cd6c66f48c74e2e2cd0b25bfbc8a36
muhammmadarfat <muhammadarfat1411@gmail.com>
2025-10-28 14:36:29 +0500

    ATM Machine

---
+++ ATM Machine/app.js
@@ -0,0 +1,31 @@
+// console.log("hello world!")
+
+// let salary = prompt("Salary agaye")
+// alert("go to ATM")
+
+// let Salerykitniaayihai = prompt("Enter your salary")
+// console.log(Salerykitniaayihai)
+
+// if(salary == 25000){
+//     console.log("Kal aayegi")
+// }
+
+// else if(salary == )
+ let atmMAchine = 50000; 
+let salary = 25000;
+let pin = prompt("Enter your pin code")
+if(pin == 12345){
+let widthdraw = prompt(" enter your widthdraw Amount")
+console.log(widthdraw)
+// let totalamount = atmMAchine - widthdraw
+// console.log(totalamount)
+
+// if(widthdraw > salary)
+//     console.log("insefficient balance")
+
+    
+}(widthdraw > atmMAchine)
+    console.log("no money avaiabe at the moment")
+
+
+
