cf4e5c2b12e486691a6ac49a9af2e5a060f3ac1f
muhammmadarfat <muhammadarfat1411@gmail.com>
2025-11-03 23:36:38 +0500

    Guess The Number Game

--- Guess num game/app.js
+++ Guess num game/app.js
@@ -4,7 +4,12 @@ let num = prompt("enter your number")
 if(num == 6){
     console.log("correct")
 }
-
+else if (num == 3){
+    console.log("Low guess hai!")
+}
+else if (num == 4){
+    console.log("bahut qareeb ho")
+}
 else if (num == 5){
     console.log("only one step more")
 }
